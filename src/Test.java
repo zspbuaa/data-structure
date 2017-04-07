45d45246c4084bc0a6b220333a7d60f668565490
zspbuaa <zspbuaa@gmail.com>
2017-04-07 16:19:22 +0800

    add main function

--- src/Test.java
+++ src/Test.java
@@ -2,14 +2,13 @@ import stack.MyStack;
 import tree.BiTree;
 import tree.BiTreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 /**
  * Created by zsp on 2017/3/28.
  */
 public class Test {
+    public static void main(String[] args){
+        BiTreeTest();
+    }
 
 
 
@@ -19,7 +18,7 @@ public class Test {
 
 
     
-    public static void BiTreeTest(String[] args){
+    public static void BiTreeTest(){
         //数据结构p129图6.9
         String v1 = "- + a # # * b # # - c # # d # # / e # # f # #";
         String v2 = "A B C # # D E # G # # F # # #";
